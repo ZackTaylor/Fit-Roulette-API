88913720bb1e4505aea203b07f77a558ec299f3b
Zack Tylor <taylor.zackory@gmail.com>
2017-10-24 00:03:13 -0500

    change table name to exercises. and seed the db with the begenning exercises.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017034051) do
+ActiveRecord::Schema.define(version: 20171024045740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "workouts", force: :cascade do |t|
+  create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.string "image"
     t.datetime "created_at", null: false
