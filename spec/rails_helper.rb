68133cd775a6de8bf258d116d7a94fbbc2830d77
Zack Tylor <taylor.zackory@gmail.com>
2017-10-27 18:51:32 -0500

    uncomment line to allow test requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,7 +47,7 @@ RSpec.configure do |config|
   # add factory girl
   config.include FactoryGirl::Syntax::Methods
 
-  # config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
